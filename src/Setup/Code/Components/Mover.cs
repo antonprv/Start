5e6be2b08507e696c8ced4a1cb854b9c744661eb
Your Name <anton.piruev.97@yandex.com>
2026-05-11 17:10:13 +0300

    Beginning to add rotation

--- src/Setup/Code/Components/Mover.cs
+++ src/Setup/Code/Components/Mover.cs
@@ -2,18 +2,23 @@ using Code.Common.FastMath;
 using Code.Services.Input;
 
 using Godot;
-
+using System;
 using ZenjexGodot;
 
 namespace Code.Components
 {
     public partial class Mover : CharacterBody3D
     {
-        [Export]
-        public float moveSpeed = 5f;
+        [ExportGroup("Movement")]
+        [Export] private float _moveSpeed = 5f;
+        [Export] private float _rotationSpeed = 6f;
+
+        [ExportGroup("References")]
+        [Export] private Node3D _playerModel;
 
         [Inject] private IInputService _inputService;
 
+        private Vector3 _moveDirection;
         private Vector2 _inputDirection;
         private bool _wantsToJump;
 
@@ -21,26 +26,40 @@ namespace Code.Components
 
         public override void _UnhandledKeyInput(InputEvent @event)
         {
-            _inputDirection = _inputService.GetInputVector();
-            _wantsToJump = _inputService.GetJumpState(@event);
+            BindInputs(@event);
+            HandleInputs();
         }
 
         public override void _PhysicsProcess(double delta)
         {
             HandleMovement();
+            HandleRotation();
             MoveAndSlide();
         }
 
+        private void BindInputs(InputEvent @event)
+        {
+            _inputDirection = _inputService.GetInputVector();
+            _wantsToJump = _inputService.GetJumpState(@event);
+        }
+
+        private void HandleInputs() => 
+            _moveDirection = new Vector3(_inputDirection.X, 0, _inputDirection.Y).Normalized();
+
         private void HandleMovement()
         {
-            if (_inputDirection.IsNearlyEqual(Vector2.Zero))
+            if (_moveDirection.IsNearlyEqual(Vector3.Zero))
             {
                 Velocity = Vector3.Zero;
                 return;
             }
 
-            Vector3 moveDirection = new Vector3(_inputDirection.X, 0, _inputDirection.Y).Normalized();
-            Velocity = moveDirection * moveSpeed;
+            Velocity = _moveDirection * _moveSpeed;
+        }
+
+        private void HandleRotation()
+        {
+            // stub
         }
     }
 }
